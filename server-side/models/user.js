eff2c61787195f5a07326edfc523dc859c340b97
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2024-10-01 17:12:51 +0700

    Setting up database

---
+++ server-side/models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.hasOne(models.Profile);
+      User.hasMany(models.FavoriteGame);
+    }
+  }
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Email is required'
+        },
+        notEmpty: {
+          msg: 'Email is required'
+        },
+        isEmail: {
+          msg: 'Email is invalid'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password is required'
+        },
+        notEmpty: {
+          msg: 'Password is required'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
